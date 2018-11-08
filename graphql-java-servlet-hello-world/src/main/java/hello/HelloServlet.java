b6ecbd977ae71255ee1203d00241f9aec2030593
Michiel Oliemans <oliemans@scopisto.com>
2018-11-08 09:48:39 +0100

    Updated hello world servlet example

--- graphql-java-servlet-hello-world/src/main/java/hello/HelloServlet.java
+++ graphql-java-servlet-hello-world/src/main/java/hello/HelloServlet.java
@@ -2,17 +2,12 @@ package hello;
 
 import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
-import graphql.execution.AsyncExecutionStrategy;
-import graphql.execution.SubscriptionExecutionStrategy;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.StaticDataFetcher;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
-import graphql.servlet.DefaultExecutionStrategyProvider;
-import graphql.servlet.DefaultGraphQLSchemaProvider;
-import graphql.servlet.ExecutionStrategyProvider;
 import graphql.servlet.GraphQLInvocationInputFactory;
 import graphql.servlet.GraphQLObjectMapper;
 import graphql.servlet.GraphQLQueryInvoker;
@@ -27,9 +22,7 @@ public class HelloServlet extends SimpleGraphQLHttpServlet {
   }
 
   private static GraphQLInvocationInputFactory invocationInputFactory() {
-    return GraphQLInvocationInputFactory
-        .newBuilder(new DefaultGraphQLSchemaProvider(createSchema()))
-        .build();
+    return GraphQLInvocationInputFactory.newBuilder(createSchema()).build();
   }
 
   private static GraphQLSchema createSchema() {
@@ -47,14 +40,7 @@ public class HelloServlet extends SimpleGraphQLHttpServlet {
   }
 
   private static GraphQLQueryInvoker queryInvoker() {
-    return GraphQLQueryInvoker.newBuilder()
-        .withExecutionStrategyProvider(executionStrategyProvider())
-        .build();
-  }
-
-  private static ExecutionStrategyProvider executionStrategyProvider() {
-    return new DefaultExecutionStrategyProvider(new AsyncExecutionStrategy(), null,
-        new SubscriptionExecutionStrategy());
+    return GraphQLQueryInvoker.newBuilder().build();
   }
 
   private static GraphQLObjectMapper objectMapper() {
