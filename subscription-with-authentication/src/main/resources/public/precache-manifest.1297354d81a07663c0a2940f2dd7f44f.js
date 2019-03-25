4916aa1da693275627fdc39631c2b246d34a5a22
Michiel Oliemans <oliemans@scopisto.com>
2019-03-25 09:11:56 +0100

    Embed example in java backend

---
+++ subscription-with-authentication/src/main/resources/public/precache-manifest.1297354d81a07663c0a2940f2dd7f44f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5796a0c8fac5db36d35c",
+    "url": "/static/js/main.3a7c3bb7.chunk.js"
+  },
+  {
+    "revision": "04fe938298726c4524c6",
+    "url": "/static/js/2.b7b6a409.chunk.js"
+  },
+  {
+    "revision": "5796a0c8fac5db36d35c",
+    "url": "/static/css/main.16ef25d6.chunk.css"
+  },
+  {
+    "revision": "5e2a52812ca151a60aa744e39e44bf1b",
+    "url": "/index.html"
+  }
+];
