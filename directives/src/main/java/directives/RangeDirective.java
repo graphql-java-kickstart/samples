899b270d3452cf0a0955421ff065df341b5ce240
Michiel Oliemans <oliemans@scopisto.com>
2020-04-05 09:05:54 +0200

    Update ra

--- directives/src/main/java/directives/RangeDirective.java
+++ directives/src/main/java/directives/RangeDirective.java
@@ -2,10 +2,9 @@ package directives;
 
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetcherFactories;
-import graphql.schema.GraphQLCodeRegistry;
 import graphql.schema.GraphQLDirective;
+import graphql.schema.GraphQLDirectiveContainer;
 import graphql.schema.GraphQLFieldDefinition;
-import graphql.schema.GraphQLFieldsContainer;
 import graphql.schema.GraphQLInputObjectField;
 import graphql.schema.GraphQLInputObjectType;
 import graphql.schema.GraphQLInputType;
@@ -17,35 +16,33 @@ public class RangeDirective implements SchemaDirectiveWiring {
   @Override
   public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
     environment.getElement().getArguments().forEach(it -> {
+      apply(it, environment);
+
       GraphQLInputType unwrappedInputType = Util.unwrapNonNull(it.getType());
-
-      GraphQLDirective directive = it.getDirective("range");
-      if (directive != null) {
-        applyDirective(directive, environment.getFieldDataFetcher(), environment.getCodeRegistry(),
-            environment.getFieldsContainer(), environment.getFieldDefinition());
-      }
-
       if (unwrappedInputType instanceof GraphQLInputObjectType) {
         GraphQLInputObjectType inputObjType = (GraphQLInputObjectType) unwrappedInputType;
 
         for (GraphQLInputObjectField inputField : inputObjType.getFieldDefinitions()) {
-          directive = inputField.getDirective("range");
-          if (directive != null) {
-            applyDirective(directive, environment.getFieldDataFetcher(), environment.getCodeRegistry(),
-                environment.getFieldsContainer(), environment.getFieldDefinition());
-          }
+          apply(inputField, environment);
         }
       }
     });
     return environment.getElement();
   }
 
-  private void applyDirective(GraphQLDirective directive, DataFetcher fieldDataFetcher, GraphQLCodeRegistry.Builder codeRegistry,
-      GraphQLFieldsContainer fieldsContainer, GraphQLFieldDefinition fieldDefinition) {
+  private void apply(GraphQLDirectiveContainer directiveContainer, SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
+    GraphQLDirective directive = directiveContainer.getDirective("range");
+    if (directive != null) {
+      applyDirective(directive, environment);
+    }
+  }
+
+  private void applyDirective(GraphQLDirective directive, SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
+
     double min = (double) directive.getArgument("min").getValue();
     double max = (double) directive.getArgument("max").getValue();
 
-    DataFetcher originalFetcher = fieldDataFetcher;
+    DataFetcher originalFetcher = environment.getFieldDataFetcher();
     DataFetcher dataFetcher = DataFetcherFactories
         .wrapDataFetcher(originalFetcher, ((dataFetchingEnvironment, value) -> {
           if (value instanceof Double && ((double) value < min || (double) value > max)) {
@@ -56,9 +53,9 @@ public class RangeDirective implements SchemaDirectiveWiring {
           return value;
         }));
 
-    codeRegistry.dataFetcher(
-        fieldsContainer,
-        fieldDefinition,
+    environment.getCodeRegistry().dataFetcher(
+        environment.getFieldsContainer(),
+        environment.getFieldDefinition(),
         dataFetcher
     );
   }
