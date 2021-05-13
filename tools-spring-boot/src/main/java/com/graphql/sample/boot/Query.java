dc58f506beee2d4ea3d288e9920af09b702bfb9a
oliemansm <oliemans@scopisto.com>
2021-05-13 21:46:04 +0200

    chore: update samples to v12.0.0-SNAPSHOT

---
+++ tools-spring-boot/src/main/java/com/graphql/sample/boot/Query.java
@@ -0,0 +1,12 @@
+package com.graphql.sample.boot;
+
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+class Query implements GraphQLQueryResolver {
+
+  Post getPost(Long id) {
+    return new Post(id);
+  }
+}
