90d71661b2110c615b5bb9a01604152b44030310
oliemansm <oliemans@scopisto.com>
2022-10-03 18:15:11 +0200

    fix: replace deprecated dataloader constructor

--- request-scoped-dataloader/src/main/java/graphql/servlet/examples/dataloader/requestscope/CustomGraphQLContextBuilder.java
+++ request-scoped-dataloader/src/main/java/graphql/servlet/examples/dataloader/requestscope/CustomGraphQLContextBuilder.java
@@ -4,11 +4,14 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import graphql.kickstart.execution.context.GraphQLKickstartContext;
 import graphql.kickstart.servlet.context.GraphQLServletContextBuilder;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.Session;
 import javax.websocket.server.HandshakeRequest;
 import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderFactory;
 import org.dataloader.DataLoaderRegistry;
 import org.springframework.stereotype.Component;
 
@@ -22,8 +25,11 @@ public class CustomGraphQLContextBuilder implements GraphQLServletContextBuilder
   }
 
   @Override
-  public GraphQLKickstartContext build(HttpServletRequest req, HttpServletResponse response) {
-    return GraphQLKickstartContext.of(buildDataLoaderRegistry());
+  public GraphQLKickstartContext build(HttpServletRequest request, HttpServletResponse response) {
+    Map<Object, Object> map = new HashMap<>();
+    map.put(HttpServletRequest.class, request);
+    map.put(HttpServletResponse.class, response);
+    return GraphQLKickstartContext.of(buildDataLoaderRegistry(), map);
   }
 
   @Override
@@ -32,14 +38,17 @@ public class CustomGraphQLContextBuilder implements GraphQLServletContextBuilder
   }
 
   @Override
-  public GraphQLKickstartContext build(Session session, HandshakeRequest request) {
-    return GraphQLKickstartContext.of(buildDataLoaderRegistry());
+  public GraphQLKickstartContext build(Session session, HandshakeRequest handshakeRequest) {
+    Map<Object, Object> map = new HashMap<>();
+    map.put(Session.class, session);
+    map.put(HandshakeRequest.class, handshakeRequest);
+    return GraphQLKickstartContext.of(buildDataLoaderRegistry(), map);
   }
 
   private DataLoaderRegistry buildDataLoaderRegistry() {
     DataLoaderRegistry dataLoaderRegistry = new DataLoaderRegistry();
     DataLoader<Integer, String> customerLoader =
-        new DataLoader<>(
+        DataLoaderFactory.newDataLoader(
             customerIds -> supplyAsync(() -> customerRepository.getUserNamesForIds(customerIds)));
     dataLoaderRegistry.register("customerDataLoader", customerLoader);
     return dataLoaderRegistry;
