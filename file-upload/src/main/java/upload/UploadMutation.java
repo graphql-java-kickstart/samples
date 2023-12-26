9fe5f93f5460c515166aae593ff418f110455e2a
Federico Rispo <dev@federicorispo.com>
2023-12-26 14:00:16 +0100

    fix: update file-upload example
    - Fixed the file-upload example: it is necessary to declare at least one
      query, otherwise the graphql-java-tools throws exceptions during the
      schema parsing
    - Replaced the return boolean type with a UploadedFile type containing
      the filename, type and the content of the textual file uploaded
    - Added a README explaining how to run it
    
    Refs: #94

--- file-upload/src/main/java/upload/UploadMutation.java
+++ file-upload/src/main/java/upload/UploadMutation.java
@@ -2,6 +2,8 @@ package upload;
 
 import graphql.kickstart.tools.GraphQLMutationResolver;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import javax.servlet.http.Part;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,9 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 class UploadMutation implements GraphQLMutationResolver {
 
-  boolean upload(Part part) throws IOException {
-    log.info("Part: {}", part.getSubmittedFileName());
-    part.write(part.getSubmittedFileName());
-    return true;
+  Map<String, String> upload(Part part) throws IOException {
+    Map<String, String> uploadedFile = Map.of(
+      "filename", part.getSubmittedFileName(),
+      "type", part.getContentType(),
+      "content", new String(part.getInputStream().readAllBytes(), StandardCharsets.UTF_8)
+    );
+    log.info("File uploaded: {}", uploadedFile);
+    return uploadedFile;
   }
 }
